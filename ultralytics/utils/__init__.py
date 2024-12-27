c7e30bb74c214407f91a336915854ed9cb4d3448
UltralyticsAssistant <web@ultralytics.com>
2024-12-27 22:20:58 +0000

    Auto-format by https://ultralytics.com

--- ultralytics/utils/__init__.py
+++ ultralytics/utils/__init__.py
@@ -34,17 +34,6 @@ RANK = int(os.getenv("RANK", -1))
 LOCAL_RANK = int(os.getenv("LOCAL_RANK", -1))  # https://pytorch.org/docs/stable/elastic/run.html
 
 
-
-
-
-
-
-
-
-
-
-
-
 # Other Constants
 ARGV = sys.argv or ["", ""]  # sometimes sys.argv = []
 FILE = Path(__file__).resolve()
